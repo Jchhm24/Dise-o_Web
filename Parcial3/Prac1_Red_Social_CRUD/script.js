1215e102d0c605217c4d207c633d90a0e95f64ba
Julio Chan <133795465+Jchhm24@users.noreply.github.com>
2023-07-04 10:18:25 -0600

    P1-Parc3-CrudSocial

--- Parcial3/Prac1_Red_Social_CRUD/script.js
+++ Parcial3/Prac1_Red_Social_CRUD/script.js
@@ -2,6 +2,7 @@ let form = document.getElementById("form");
 let textarea = document.getElementById("textarea");
 let msg = document.getElementById("msg");
 let post = document.getElementById("post");
+let fecha=document.getElementById("date");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -25,16 +26,20 @@ let acceptData = () => {
 };
 
 let createPost = () => {
+  let currentDate = new Date().toLocaleString(); // Obtener la fecha actual
+  fecha.innerHTML = "Fecha: " + currentDate.toString();
+
   post.innerHTML += `
-  <div class="public">
-        <p class="comentario">${data.text}</p>
-        <span class="options">
-            <i onclick="editpost(this)" class="fas fa-edit"></i>
-            <i onclick="deletepost(this)" class="fas fa-trash-alt"></i>
-        </span>
+    <div class="public">
+      <p id="date">${currentDate.toString()}</p>
+      <p class="comentario">${data.text}</p>
+      <span class="options">
+        <i onclick="editpost(this)" class="fas fa-edit"></i>
+        <i onclick="deletepost(this)" class="fas fa-trash-alt"></i>
+      </span>
     </div>
-    `
-    textarea.value = "";
+  `;
+  textarea.value = "";
 };
 
 
